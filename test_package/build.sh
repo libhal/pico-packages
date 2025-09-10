c5dc0110852009744e9e9b53d040df6af0f49cc0
Shin Umeda <umeda.shin@gmail.com>
2025-09-10 11:46:11 -0700

    disable LTO because __wrap_puts is weird with lto

--- test_package/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-set -ex
-
-
-conan install . -pr:h=../profiles/rp2350 -pr:h=arm-gcc-12.3 --build=missing -of build
-conan build . -pr:h=../profiles/rp2350 -pr:h=arm-gcc-12.3 -of build
---
+++ test_package/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+set -ex
+
+conan build . -pr:h=../profiles/rp2350 -pr:h=arm-gcc-12.3 -nr --build=missing  -of build
+
